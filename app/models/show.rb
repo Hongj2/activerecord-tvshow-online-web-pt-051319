f6e6be6cb54bf42e6ee5abd84514c48906763e82
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-07-31 22:50:31 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,6 +4,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
+    self.where("rating= ?", self.highest_rating).first)
   end
   
   def self.lowest_rating 
@@ -11,7 +12,7 @@ class Show < ActiveRecord::Base
   end
   
   def self.least_popular_show
-
+self.where("rating= ?", self.lowest_rating).first)
   end
   
   def self.ratings_sum
